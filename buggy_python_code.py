c3b52b4f63e963a3c59dd6d7733bcf04c4d22b05
Lucie Krenkova <525029@mail.muni.cz>
2023-03-27 12:51:39 +0200

    second

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -14,7 +14,7 @@ def index():
 
         
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
-class Person(object):
+class Person():
     def __init__(self, name):
         self.name = name
 
@@ -30,7 +30,7 @@ def fetch_website(urllib_version, url):
  
     try: 
         http = urllib.PoolManager()
-        r = http.request('GET', url)
+        http.request('GET', url)  # r = http.request('GET', url)
     except:
         print('Exception')
 
